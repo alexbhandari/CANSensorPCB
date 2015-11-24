6b22f0c06d9e1e6db99569691d3195e72c67ae48
Alex Bhandari-Young <alexbhandari@gmail.com>
2015-11-24 09:15:06 -0500

    changed folder name

---
+++ CAN_Example/Debug/source/pinmux.pp
@@ -0,0 +1,25 @@
+# FIXED
+
+source/pinmux.obj: ../source/pinmux.c
+source/pinmux.obj: C:/Users/Zoltan/TI_Workspace/CAN_example/include/pinmux.h
+source/pinmux.obj: C:/Users/Zoltan/TI_Workspace/CAN_example/include/reg_pinmux.h
+source/pinmux.obj: C:/Users/Zoltan/TI_Workspace/CAN_example/include/sys_common.h
+source/pinmux.obj: C:/Users/Zoltan/TI_Workspace/CAN_example/include/hal_stdtypes.h
+source/pinmux.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h
+source/pinmux.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h
+source/pinmux.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h
+source/pinmux.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+source/pinmux.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h
+source/pinmux.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h
+
+../source/pinmux.c: 
+C:/Users/Zoltan/TI_Workspace/CAN_example/include/pinmux.h: 
+C:/Users/Zoltan/TI_Workspace/CAN_example/include/reg_pinmux.h: 
+C:/Users/Zoltan/TI_Workspace/CAN_example/include/sys_common.h: 
+C:/Users/Zoltan/TI_Workspace/CAN_example/include/hal_stdtypes.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h: 
---
+++ CAN_Example/Debug/source/sys_main.pp
@@ -0,0 +1,39 @@
+# FIXED
+
+source/sys_main.obj: ../source/sys_main.c
+source/sys_main.obj: C:/Users/Zoltan/TI_Workspace/CAN_example/include/sys_common.h
+source/sys_main.obj: C:/Users/Zoltan/TI_Workspace/CAN_example/include/hal_stdtypes.h
+source/sys_main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h
+source/sys_main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h
+source/sys_main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h
+source/sys_main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+source/sys_main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h
+source/sys_main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h
+source/sys_main.obj: C:/Users/Zoltan/TI_Workspace/CAN_example/include/can.h
+source/sys_main.obj: C:/Users/Zoltan/TI_Workspace/CAN_example/include/reg_can.h
+source/sys_main.obj: C:/Users/Zoltan/TI_Workspace/CAN_example/include/esm.h
+source/sys_main.obj: C:/Users/Zoltan/TI_Workspace/CAN_example/include/reg_esm.h
+source/sys_main.obj: C:/Users/Zoltan/TI_Workspace/CAN_example/include/sys_core.h
+source/sys_main.obj: C:/Users/Zoltan/TI_Workspace/CAN_example/include/sci.h
+source/sys_main.obj: C:/Users/Zoltan/TI_Workspace/CAN_example/include/reg_sci.h
+source/sys_main.obj: C:/Users/Zoltan/TI_Workspace/CAN_example/include/reg_gio.h
+source/sys_main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdlib.h
+
+../source/sys_main.c: 
+C:/Users/Zoltan/TI_Workspace/CAN_example/include/sys_common.h: 
+C:/Users/Zoltan/TI_Workspace/CAN_example/include/hal_stdtypes.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h: 
+C:/Users/Zoltan/TI_Workspace/CAN_example/include/can.h: 
+C:/Users/Zoltan/TI_Workspace/CAN_example/include/reg_can.h: 
+C:/Users/Zoltan/TI_Workspace/CAN_example/include/esm.h: 
+C:/Users/Zoltan/TI_Workspace/CAN_example/include/reg_esm.h: 
+C:/Users/Zoltan/TI_Workspace/CAN_example/include/sys_core.h: 
+C:/Users/Zoltan/TI_Workspace/CAN_example/include/sci.h: 
+C:/Users/Zoltan/TI_Workspace/CAN_example/include/reg_sci.h: 
+C:/Users/Zoltan/TI_Workspace/CAN_example/include/reg_gio.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdlib.h: 
